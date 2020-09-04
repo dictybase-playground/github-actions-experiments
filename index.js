4cac5e3df8cb1e61262bb1c76c3e67a35993f622
wildlifehexagon <crossthebreeze@gmail.com>
2020-09-03 20:48:42 -0500

    fix: fix path

--- index.js
+++ index.js
@@ -6,19 +6,17 @@ try {
   console.log(`The event payload: ${payloadJSON}`)
   const payload = github.context.payload
 
-  if (payload.client_payload.github.slash_command.args === undefined) {
+  if (payload.client_payload.slash_command.args === undefined) {
     process.exit(1)
   }
 
-  const cluster = payload.client_payload.github.slash_command.args.named.cluster
-  const unnamedArgs =
-    payload.client_payload.github.slash_command.args.unnamed.all
-  const branch = payload.client_payload.github.slash_command.args.named.branch
-  const commit = payload.client_payload.github.slash_command.args.named.commit
-  const pullRequestID =
-    payload.client_payload.github.slash_command.args.named.pr
-  const sha = payload.client_payload.github.pull_request.head.sha
-  const pullRequestNumber = payload.client_payload.github.pull_request.number
+  const cluster = payload.client_payload.slash_command.args.named.cluster
+  const unnamedArgs = payload.client_payload.slash_command.args.unnamed.all
+  const branch = payload.client_payload.slash_command.args.named.branch
+  const commit = payload.client_payload.slash_command.args.named.commit
+  const pullRequestID = payload.client_payload.slash_command.args.named.pr
+  const sha = payload.client_payload.pull_request.head.sha
+  const pullRequestNumber = payload.client_payload.pull_request.number
   let shortSHA = commit.substr(0, 7)
   let ref = commit
   let imageTag = commit
